b7fb4876ee3910e217ca633f71c48a952bd48d8f
kirilldikalin <dik.kir200010@gmail.com>
2025-03-20 06:41:09 +0300

    add_prices

---
+++ platform/storage/migrations/20252003_create_prices_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS prices (
+    id INT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    category VARCHAR(255) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    create_date TIMESTAMP WITH TIME ZONE NOT NULL
+);
