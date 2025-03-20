200fe66c9370d725caaad956f64680ceb95c0b9f
kirilldikalin <dik.kir200010@gmail.com>
2025-03-20 06:09:12 +0300

    fix_main

--- main.go
+++ main.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"log"
+
+	"project_sem/internal/server"
+	"project_sem/platform/config"
+)
+
+func main() {
+	settings, err := config.Load("")
+	if err != nil {
+		log.Fatalf("failed to load configuration: %v", err)
+	}
+
+	app, err := server.New(settings)
+	if err != nil {
+		log.Fatalf("failed to initialize server: %v", err)
+	}
+
+	app.Run()
+}
